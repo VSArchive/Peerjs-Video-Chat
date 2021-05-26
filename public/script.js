7b4a38fb16664bcc73a379397ed92e83e69f598a
Vineel Sai <vineelsai26@gmail.com>
2021-05-26 11:12:53 +0530

    proper exit and send button to chat

--- public/script.js
+++ public/script.js
@@ -5,15 +5,15 @@ const myPeer = new Peer(undefined, {
     host: '/',
     port: '443'
 })
-let myVideoStream;
+let myVideoStream
 const myVideo = document.createElement('video')
-myVideo.muted = true;
+myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    myVideoStream = stream;
+    myVideoStream = stream
     addVideoStream(myVideo, stream)
     myPeer.on('call', call => {
         call.answer(stream)
@@ -27,19 +27,28 @@ navigator.mediaDevices.getUserMedia({
         connectToNewUser(userId, stream)
     })
     // input value
-    let text = $("input");
+    let text = $("input")
     // when press enter send message
     $('html').keydown(function (e) {
         if (e.which == 13 && text.val().length !== 0) {
-            socket.emit('message', text.val());
+            socket.emit('message', text.val())
             text.val('')
         }
-    });
+    })
+
     socket.on("createMessage", message => {
-        $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+        $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`)
         scrollToBottom()
     })
 })
+
+function sendMessage() {
+    let text = $("input")
+    if (text.val().length !== 0) {
+        socket.emit('message', text.val())
+        text.val('')
+    }
+}
 
 socket.on('user-disconnected', userId => {
     if (peers[userId]) peers[userId].close()
@@ -70,34 +79,31 @@ function addVideoStream(video, stream) {
     videoGrid.append(video)
 }
 
-
-
 const scrollToBottom = () => {
-    var d = $('.main__chat_window');
-    d.scrollTop(d.prop("scrollHeight"));
+    var d = $('.main__chat_window')
+    d.scrollTop(d.prop("scrollHeight"))
 }
 
-
 const muteUnmute = () => {
-    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    const enabled = myVideoStream.getAudioTracks()[0].enabled
     if (enabled) {
-        myVideoStream.getAudioTracks()[0].enabled = false;
-        setUnmuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = false
+        setUnmuteButton()
     } else {
-        setMuteButton();
-        myVideoStream.getAudioTracks()[0].enabled = true;
+        setMuteButton()
+        myVideoStream.getAudioTracks()[0].enabled = true
     }
 }
 
 const playStop = () => {
     console.log('object')
-    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    let enabled = myVideoStream.getVideoTracks()[0].enabled
     if (enabled) {
-        myVideoStream.getVideoTracks()[0].enabled = false;
+        myVideoStream.getVideoTracks()[0].enabled = false
         setPlayVideo()
     } else {
         setStopVideo()
-        myVideoStream.getVideoTracks()[0].enabled = true;
+        myVideoStream.getVideoTracks()[0].enabled = true
     }
 }
 
@@ -106,7 +112,7 @@ const setMuteButton = () => {
     <i class="fas fa-microphone"></i>
     <span>Mute</span>
   `
-    document.querySelector('.main__mute_button').innerHTML = html;
+    document.querySelector('.main__mute_button').innerHTML = html
 }
 
 const setUnmuteButton = () => {
@@ -114,7 +120,7 @@ const setUnmuteButton = () => {
     <i class="unmute fas fa-microphone-slash"></i>
     <span>Unmute</span>
   `
-    document.querySelector('.main__mute_button').innerHTML = html;
+    document.querySelector('.main__mute_button').innerHTML = html
 }
 
 const setStopVideo = () => {
@@ -122,7 +128,7 @@ const setStopVideo = () => {
     <i class="fas fa-video"></i>
     <span>Stop Video</span>
   `
-    document.querySelector('.main__video_button').innerHTML = html;
+    document.querySelector('.main__video_button').innerHTML = html
 }
 
 const setPlayVideo = () => {
@@ -130,9 +136,9 @@ const setPlayVideo = () => {
   <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
   `
-    document.querySelector('.main__video_button').innerHTML = html;
+    document.querySelector('.main__video_button').innerHTML = html
 }
 
 const leave = () => {
-    location.href = '/exit'
+    location.href = 'https://cohelp-app.herokuapp.com/'
 }
